fbf2b5f0271e2fed9faef3d1f5d52b01aca82182
sCima <44555125+sCima@users.noreply.github.com>
2023-09-27 17:43:19 -0300

    Adicionado um botão para voltar ao topo

--- js/app.js
+++ js/app.js
@@ -1,4 +1,19 @@
-function someTitulo() {
-    let demir = document.getElementById('demirval');
+// Pegar o botão
+let botao = document.getElementById("topo");
+
+// Quando o usuário scrollar 20px para baixo, o botão aparece
+window.onscroll = function() {scrollBotao()};
+
+function scrollBotao() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    botao.style.display = "block";
+  } else {
+    botao.style.display = "none";
+  }
 }
 
+// Quando o usuário clicar, voltar ao topo
+function voltaTopo() {
+  document.body.scrollTop = 0; // Para Safari
+  document.documentElement.scrollTop = 0; // Outros navegadores
+}
